e0dd773ccefa6fab30f3ad4755e0b9693e4121af
James Gordon <gordo@Jamess-MBP.PK5001Z>
2015-01-20 12:06:40 -0600

    Removed all outputs, added output script, re-ran outputs

---
+++ sql/outputs.sql
@@ -0,0 +1,162 @@
+-- output properties appended table
+COPY (
+SELECT *
+FROM properties_appd
+) to '/Users/gordo/multco_prop_taxes/output/properties_appd.csv' DELIMITER ',' CSV HEADER;
+
+-- output cash matches table
+COPY (
+SELECT *
+FROM cash_sales_matches
+) to '/Users/gordo/multco_prop_taxes/output/cash_sales_matches.csv' DELIMITER ',' CSV HEADER;
+
+-- output unmatched cash sales
+COPY (
+SELECT *
+FROM cash_sales_orig
+WHERE id NOT IN (SELECT cash_sales_id FROM cash_sales_matches)
+AND propertyid IS NOT NULL
+) to '/Users/gordo/multco_prop_taxes/output/cash_sales_unmatched.csv' DELIMITER ',' CSV HEADER;
+
+-- output flat file all sales
+COPY (
+SELECT 	  
+	  b.id as prop_sales_id
+	, a.property_id
+	, date_sale
+	, consideration_amount
+	, deed
+	, seller
+	, buyer
+	, instrument
+	, street_no
+        , street_dir
+        , street
+        , street_typ
+        , unit_no
+        , zip
+        , address
+        , city
+        , state
+        , state_id
+        , rno
+        , owner_names
+        , owner1
+        , owner2
+        , owner3
+        , owneraddr
+        , ownercity
+        , ownerstate
+        , ownerzip
+        , legal_desc
+        , taxcode
+        , prop_code
+        , prpcd_desc
+        , landuse
+        , yearbuilt
+        , bldgsqft
+        , bedrooms
+        , floors
+        , units
+        , mktvalyr1
+        , landval1
+        , bldgval1
+        , totalval1
+        , mktvalyr2
+        , landval2
+        , bldgval2
+        , totalval2
+        , mktvalyr3
+        , landval3
+        , bldgval3
+        , totalval3
+        , acc_status
+        , a_t_sqft
+        , a_t_acres
+        , frontage
+        , county
+        , source
+        , tlid
+FROM properties_appd a
+JOIN property_sales b
+ON a.property_id = b.property_id
+) to '/Users/gordo/multco_prop_taxes/output/flat_file_all_sales.csv' DELIMITER ',' CSV HEADER;
+
+-- output flat file cash only
+COPY (
+SELECT 	  
+	  cash_sales_id
+	, prop_sales_id
+	, a.property_id
+	, date_coe
+	, date_sale
+	, date_diff
+	, consideration_amount
+	, deed
+	, seller
+	, buyer
+	, instrument
+	, a.street_no
+        , a.street_dir
+        , a.street
+        , a.street_typ
+        , a.unit_no
+        , a.zip
+        , address
+        , city
+        , state
+        , state_id
+        , rno
+        , owner_names
+        , owner1
+        , owner2
+        , owner3
+        , owneraddr
+        , ownercity
+        , ownerstate
+        , ownerzip
+        , legal_desc
+        , taxcode
+        , prop_code
+        , prpcd_desc
+        , landuse
+        , yearbuilt
+        , bldgsqft
+        , bedrooms
+        , floors
+        , units
+        , mktvalyr1
+        , landval1
+        , bldgval1
+        , totalval1
+        , mktvalyr2
+        , landval2
+        , bldgval2
+        , totalval2
+        , mktvalyr3
+        , landval3
+        , bldgval3
+        , totalval3
+        , acc_status
+        , a_t_sqft
+        , a_t_acres
+        , frontage
+        , county
+        , source
+        , tlid
+FROM properties_appd a
+JOIN cash_sales_matches b
+ON a.property_id = b.property_id
+) to '/Users/gordo/multco_prop_taxes/output/flat_file_cash_only.csv' DELIMITER ',' CSV HEADER;
+
+-- output cash buyers
+COPY TO (
+SELECT *
+FROM cash_buyers
+)  to '/Users/gordo/multco_prop_taxes/output/cash_buyers.csv' DELIMITER ',' CSV HEADER;
+
+-- output cash sellers
+COPY TO (
+SELECT *
+FROM cash_sellers
+)  to '/Users/gordo/multco_prop_taxes/output/cash_sellers.csv' DELIMITER ',' CSV HEADER;
